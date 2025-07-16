86216902213f9c651449a89ad1d52f6a3108ece5
NishitShelar <nishitsshelar@gmail.com>
2025-07-16 10:22:12 +0530

    updated backend index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,10 +29,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // Serve frontend in production
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
+app.use(express.static(path.join(__dirname, "../dist")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 // Start server
